702806359ed76ca12651d5627b50530cc908e1e3
Mykhailo Skrypkin <mykhailo.skrypkin.ext@dkv-mobility.com>
2024-05-07 23:17:03 +0300

    Fix size

--- resources/views/modals.blade.php
+++ resources/views/modals.blade.php
@@ -1,7 +1,7 @@
 <div id="laravel-livewire-modals" tabindex="-1"
     data-bs-backdrop="static" data-bs-keyboard="true"
     wire:ignore.self class="modal fade">
-        <div class="modal-dialog {{$size ?? "modal-lg"}}">
+        <div class="modal-dialog @isset($size){{$size }}@endisset">
             @if($alias)
                 @livewire($alias, $params, key($activeModal))
             @endif
